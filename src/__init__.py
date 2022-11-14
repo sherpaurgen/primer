62e5306d472f49c6c6ddfcb544ba95d4cf3f7aa3
ush <sherpaurgen@users.noreply.github.com>
2022-11-14 17:36:01 +0545

    initalize sqlite db

--- src/__init__.py
+++ src/__init__.py
@@ -2,16 +2,19 @@ from flask import Flask,jsonify
 import os
 from src.auth import auth
 from src.bookmarks import bookmarks
-
+from src.database import db
 def create_app(test_config=None):
     app=Flask(__name__,instance_relative_config=True)
     if test_config is None:
         app.config.from_mapping(
-            os.environ.get("SECRET_KEY")
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI")
+
         )
     else:
         app.config(test_config)   
-
+    db.app=app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
     return app    
